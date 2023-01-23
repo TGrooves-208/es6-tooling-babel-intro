c84c493a6a44d16ee5d97295e508eb2160137b5c
GAguilar208 <gaguilar208.accessibility@gmail.com>
2023-01-22 21:20:51 -0700

    removed node_modules folder

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// We are going to use Babel to convert this to code for all browsers
+// We can use npm to run the script we have added in our package.json file
+// The command to accomplish is "npm run babel"
+var x = 1;
---
+++ index.js
@@ -0,0 +1,4 @@
+// We are going to use Babel to convert this to code for all browsers
+// We can use npm to run the script we have added in our package.json file
+// The command to accomplish is "npm run babel"
+const x = 1;
